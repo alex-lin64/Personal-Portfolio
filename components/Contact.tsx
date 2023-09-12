4ff92aa4aab64f9120c6559299b0cfce046acf0a
alex-lin64 <alex_lin@brown.edu>
2023-09-11 23:03:45 -0400

    eslint

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
         items-center justify-center'>
       <p className='font-titleFont text-lg text-textGreen font-semibold flex 
         items-center tracking-wide'>
-        0.4 What&apos;s Next?
+        0.4 What's Next?
       </p>
       <h2 className='font-titleFont text-5xl font-semibold'>Get in Touch</h2>
       <span className='text-[#778DA9] text-lg sm:text-xl leading-[1]'>
@@ -17,7 +17,7 @@ const Contact = () => {
       </span>
       <p className='max-w-[600px] text-center text-textDark'>
         I am currently looking for roles in software engineering, systems engineering,
-        and ML/AI.  Always feel free to reach out, I&apos;d love to chat!
+        and ML/AI.  Always feel free to reach out, I'd love to chat!
       </p>
       <a href='mailto:alex_lin@brown.edu'>
         <button className='w-40 h-14 border border-textGreen mt-6 font-titleFont 
